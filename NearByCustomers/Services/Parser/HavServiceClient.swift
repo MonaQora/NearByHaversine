bb1dd6a37d77c9d3ad56c562409499176e32c05d
Mona Qora <mona.abdelrahim@vodafone.com>
2020-01-27 01:40:06 +0200

    Service Client

--- NearByCustomers/Services/Parser/HavServiceClient.swift
+++ NearByCustomers/Services/Parser/HavServiceClient.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class HavServiceClient: NSObject {
-
+struct ResourceList<T> {
+    let urlString: String
+    let parse: (Data) -> [T]?
 }
+
+class ServiceClient: NSObject {
+    func load<T>(resource: ResourceList<T>, completion: @escaping ((Response<[T], ErrorResponse>) -> Void)) {
+        var url:URL?
+        if resource.urlString.isEmpty || (URL(string: resource.urlString) == nil){
+            completion(Response.failure(ErrorResponse.custom(string: ErrorMessage.wrongURLMessage)))
+            return
+        }
+        else {
+             url = URL(string: resource.urlString)!
+             let sessionConfig = URLSessionConfiguration.default
+             sessionConfig.timeoutIntervalForRequest = 30.0
+            sessionConfig.timeoutIntervalForResource = 30.0
+             let session = URLSession(configuration: sessionConfig)
+            session.dataTask(with: url!) { data, response, error in
+                if let error = error {
+                    completion(Response.failure(ErrorResponse.native(string: error.localizedDescription)))
+                }
+                else if let data = data  {
+                    if (resource.parse(data) != nil) {
+                        let list = resource.parse(data)!
+                        completion(Response.success(list))
+                    }
+                    else {
+                        completion(Response.failure(ErrorResponse.custom(string: ErrorMessage.unidentifiedResponseMessage)))
+                    }
+                }
+                else {
+                    completion(Response.failure(ErrorResponse.custom(string: ErrorMessage.noDataMessage)))
+                }
+                
+            }.resume()
+        }
+    }
+}
+
